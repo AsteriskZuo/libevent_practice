0fd8ad2dcd81ad6409b17295afad4deedfcffecf
asteriskzuo <zuoyuhsywn@hotmail.com>
2020-06-09 16:11:36 +0800

    update

---
+++ test_libevent/test_libevent/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  test_libevent
+//
+//  Created by zuoyu on 2020/6/8.
+//  Copyright © 2020 zuoyu. All rights reserved.
+//
+
+#include <iostream>
+#include <thread>
+
+#include "preface.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    test_example_03(0, 0);
+    return 0;
+}
---
+++ test_libevent/test_libevent/preface.hpp
@@ -0,0 +1,18 @@
+//
+//  preface.hpp
+//  test_libevent
+//
+//  Created by zuoyu on 2020/6/9.
+//  Copyright © 2020 zuoyu. All rights reserved.
+//
+
+#ifndef preface_hpp
+#define preface_hpp
+
+#include <stdio.h>
+
+int test_example_01(int c, char **v);
+int test_example_02(int c, char **v);
+int test_example_03(int c, char **v);
+
+#endif /* preface_hpp */
